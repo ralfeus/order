97d40703c63c49fd72bfbcf3989ab754dd591653
z52657 <nadiya.kozoriz@cz.ibm.com>
2020-07-30 11:44:18 +0200

    user managments

--- app/models/user.py
+++ app/models/user.py
@@ -19,12 +19,14 @@ class User(db.Model, UserMixin):
     username = Column(String(32), unique=True, nullable=False)
     email = Column(String(80))
     password_hash = Column(String(200))
+    enabled = Column(db.Boolean(), nullable=False)
 
-    # Business
-    balance = Column(Integer, default=0)
-
+    # User information
+    # enabled = Column('is_enabled', db.Boolean(), nullable=False)
     when_created = Column(DateTime)
     when_changed = Column(DateTime)
+    # Business
+    balance = Column(Integer, default=0)
 
     def get_id(self):
         return str(self.id)
