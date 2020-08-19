d9a3dc90fb68c1c32ff50c827fe68d2166694b41
ralfeus <ralfeus@gmail.com>
2020-08-19 07:37:08 +0200

    flask-security

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 '''
 User model
 '''
-from flask_login import UserMixin
+from flask_security import UserMixin
 from sqlalchemy import Boolean, Column, DateTime, Integer, String
 from werkzeug.security import check_password_hash, generate_password_hash
 
