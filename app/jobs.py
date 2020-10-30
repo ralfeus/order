e994ec0580a582aa905cb7f4b3875ea4e4461ef7
ralfeus <ralfeus@gmail.com>
2020-10-30 20:29:14 +0100

    stop beat

--- app/jobs.py
+++ app/jobs.py
@@ -66,10 +66,10 @@ def import_products():
                                 modified: {modified}, ignored: {ignored}""")
     db.session.commit()
 
-@celery.on_after_finalize.connect
-def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(3600, update_purchase_orders_status,
-        name='Update PO status every 60 minutes')
+# @celery.on_after_finalize.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     sender.add_periodic_task(3600, update_purchase_orders_status,
+#         name='Update PO status every 60 minutes')
 
 
 @celery.task
