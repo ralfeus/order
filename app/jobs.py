5486307ac8e57d1242dfd68ed203c9869a6957e6
ralfeus <ralfeus@gmail.com>
2020-11-10 12:15:32 +0100

    increase PO status update interval

--- app/jobs.py
+++ app/jobs.py
@@ -70,7 +70,7 @@ def import_products():
 
 @celery.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(3600, update_purchase_orders_status,
+    sender.add_periodic_task(7200, update_purchase_orders_status,
         name='Update PO status every 60 minutes')
 
 
