780a225637b74ada5f4db941f8e13c7d128c696d
Maryna <marinaleych23@gmail.com>
2021-01-21 23:32:12 +0200

    test

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Config:
     LOG_LEVEL = logging.INFO
     SECRET_KEY = '150000$ZdGZazdW$5310a52465082ab169c8b62438fc09d2f69b2c5bf3945c0b99bdb1e082ae4f79'
     ADMIN_HASH = '1500004ci4LtPz8641fd1956415e82846cddecb7a419e1fecc34b6e73b3dd6e22a1a943dce9a4a'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://talya:talya@sandlet/talya?auth_plugin=mysql_native_password'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://maryna:maryna@localhost/order-master?auth_plugin=mysql_native_password'
     # SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://dina:dina@mgmt-2.ralfeus.eu/dina?auth_plugin=mysql_native_password'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
