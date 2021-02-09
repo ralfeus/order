909c6fc7b37bbd8c4d51cc9fa11151ba71dc1210
ralfeus <ralfeus@gmail.com>
2021-02-09 15:11:30 +0100

    #74 final

--- app/modules/crisp.py
+++ app/modules/crisp.py
@@ -1,9 +1,10 @@
-from flask.app import Flask
-
+from flask import Flask
 
 def init(app: Flask):
-    with app.app_context():
-        from app.settings.models.setting import Setting
-        crisp_id = Setting.query.get('crisp.id')
-        app.jinja_env.globals['crisp_id'] = crisp_id.value \
-            if crisp_id else None
+    @app.before_request
+    def set_crisp_id():
+        with app.app_context():
+            from app.settings.models.setting import Setting
+            crisp_id = Setting.query.get('crisp.id')
+            app.jinja_env.globals.update(
+                crisp_id=crisp_id.value if crisp_id else None)
