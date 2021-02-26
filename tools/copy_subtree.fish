8049aef9efee755833a73bc6f4fceab4c6b406df
ralfeus <ralfeus@gmail.com>
2021-02-26 20:21:50 +0100

    copy subtree fix

--- tools/copy_subtree.fish
+++ tools/copy_subtree.fish
@@ -4,6 +4,6 @@ cd /var/www/order
 find tenants -maxdepth 1 -mindepth 1 -exec basename {} \; | \
     while read tenant
         echo $tenant
-        set PYTHONPATH=/var/www/order/tenants/mihwa
+        set PYTHONPATH=/var/www/order/tenants/$tenant
         celery -A tenants.$tenant.main call app.network.jobs.copy_subtree
     end
