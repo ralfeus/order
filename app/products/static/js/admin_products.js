03640c6ffd4ab81c055595a6af58fde47b87f975
ralfeus <ralfeus@gmail.com>
2021-03-11 13:15:32 +0100

    #47

--- app/products/static/js/admin_products.js
+++ app/products/static/js/admin_products.js
@@ -71,7 +71,7 @@ $(document).ready( function () {
             {
                 label: "Color",
                 name: 'appearance.color',
-                type: 
+                type: 'colorpicker'
             }
         ]
     });
