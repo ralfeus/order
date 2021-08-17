f5435c73a752bdcdd03258df9ad8338248dec962
ralfeus <ralfeus@gmail.com>
2021-08-17 10:21:48 +0200

    mark orders as can_be_paid regardless amount

--- app/payments/models/payment.py
+++ app/payments/models/payment.py
@@ -96,17 +96,17 @@ class Payment(db.Model, BaseModel):
             lambda acc, o: acc + o.total_krw,
             self.orders.filter_by(status=OrderStatus.pending), 0
         )
-        if self.amount_received_krw >= total_orders_amount:
-            logger.debug("Received payment amount %s is larger than total amount of related orders %s. Marking orders as can_be_paid",
-                         self.amount_received_krw, total_orders_amount)
-            for order in self.orders:
-                order.payment_method_id = self.payment_method_id
-                if order.status == OrderStatus.pending:
-                    order.set_status(OrderStatus.can_be_paid, actor=self.changed_by)
-                    messages.append(f"Order <{order.id}> status is set to CAN_BE_PAID")
-        else:
-            logger.debug("Received payment amount %s is less than total amount of related orders %s.",
-                         self.amount_received_krw, total_orders_amount)
+#        if self.amount_received_krw >= total_orders_amount:
+        logger.debug("Received payment amount %s is larger than total amount of related orders %s. Marking orders as can_be_paid",
+                     self.amount_received_krw, total_orders_amount)
+        for order in self.orders:
+            order.payment_method_id = self.payment_method_id
+            if order.status == OrderStatus.pending:
+                order.set_status(OrderStatus.can_be_paid, actor=self.changed_by)
+                messages.append(f"Order <{order.id}> status is set to CAN_BE_PAID")
+#        else:
+#            logger.debug("Received payment amount %s is less than total amount of related orders %s.",
+#                         self.amount_received_krw, total_orders_amount)
 
     def execute_payment(self):
         '''
