7621d82f04baf05bfea3cfd2cba840bddf096744
ralfeus <ralfeus@gmail.com>
2021-09-08 13:22:53 +0200

    notification fix

--- app/notifications/modules/notifications.py
+++ app/notifications/modules/notifications.py
@@ -1,9 +1,11 @@
+import logging
 from flask import Flask, request
 from flask_security import current_user
 
 def init(app: Flask):
     @app.before_request
     def add_notification_block():
+        logger = logging.getLogger('add_notification_block')
         if not request.full_path.startswith('/api') and \
            not request.full_path.startswith('/admin'):
             from app.notifications.models.notification import Notification
@@ -11,6 +13,8 @@ def init(app: Flask):
                 if not current_user.is_anonymous else 0
             unread_notifications = Notification.query.\
                 filter(Notification.id > last_read_notification).count()
+            logger.debug("There are %s unread notifications for %s",
+                unread_notifications, current_user)
             app.jinja_env.globals.update(
                 extension_1='badge.jinja',
                 notifications=unread_notifications)
