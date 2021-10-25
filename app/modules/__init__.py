62acdced11ae370994890cda19d6a43ca1724c81
ralfeus <ralfeus@gmail.com>
2021-10-25 14:17:10 +0200

    Змінити порядок роботи складу #116

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -2,10 +2,13 @@
 from importlib import import_module
 import os, os.path
 
+from flask import Flask
+
 from app.settings.models.setting import Setting
 
-def init(app):
+def init(app: Flask):
     '''Initializes all modules'''
+    logger = app.logger.getChild('modules.init()')
     modules_dir = os.path.dirname(__file__)
     files = os.listdir(modules_dir)
     for file in files:
@@ -14,6 +17,7 @@ def init(app):
         module_name = os.path.splitext(file)[0]
         try:
             if app.config['MODULES'][module_name]['enabled']:
+                logger.info("Loading module: %s", module_name)
                 module = import_module(__name__ + '.' + module_name)
                 module.init(app)
         except KeyError:
