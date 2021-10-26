4ca88bb7d275b83a301606a7ec7f7a09333778c2
ralfeus <ralfeus@gmail.com>
2021-10-26 09:28:31 +0200

    fix notifications

--- app/notifications/modules/notifications.py
+++ app/notifications/modules/notifications.py
@@ -1,22 +1,25 @@
 import logging
-from flask import Flask, request
+from flask import Flask, current_app, request
 from flask_security import current_user
 
 def init(app: Flask):
-    @app.before_request
-    def add_notification_block():
-        logger = logging.getLogger('add_notification_block')
-        if not request.full_path.startswith('/api') and \
-           not request.full_path.startswith('/admin') and \
-           not request.full_path.startswith('/static'):
-            logger.debug(request.full_path)
-            from app.notifications.models.notification import Notification
-            last_read_notification = current_user.get_profile().get('last_read_notification', 0) \
-                if not current_user.is_anonymous else 0
-            unread_notifications = Notification.query.\
-                filter(Notification.id > last_read_notification).count()
-            logger.debug("There are %s unread notifications for %s",
-                unread_notifications, current_user)
-            app.jinja_env.globals.update(
-                extension_1='badge.jinja',
-                notifications=unread_notifications)
+    app.before_request(_add_notification_block)
+
+def _add_notification_block():
+    logger = logging.getLogger('add_notification_block')
+    if not request.full_path.startswith('/api') and \
+        not request.full_path.startswith('/admin') and \
+        not request.full_path.startswith('/static') and \
+        not request.full_path.startswith('/favicon.ico') and \
+        not request.full_path.startswith('/_debug_toolbar'):
+        logger.debug(request.full_path)
+        from app.notifications.models.notification import Notification
+        last_read_notification = current_user.get_profile().get('last_read_notification', 0) \
+            if not current_user.is_anonymous else 0
+        unread_notifications = Notification.query.\
+            filter(Notification.id > last_read_notification).count()
+        logger.debug("There are %s unread notifications for %s",
+            unread_notifications, current_user)
+        current_app.jinja_env.globals.update(
+            extension_1='badge.jinja',
+            notifications=unread_notifications)
