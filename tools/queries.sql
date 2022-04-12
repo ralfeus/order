467558520fb14e6a7a0c12f3be2151c2a5b30449
ralfeus <ralfeus@gmail.com>
2022-04-12 11:44:52 +0200

    Update queries.sql

--- tools/queries.sql
+++ tools/queries.sql
@@ -25,3 +25,13 @@ with recursive cte (destination, weight, rate, shipping_method_id) as (
     union
     select 'ru', weight + 200, rate + 1800, 11 from cte where weight < 1000000
 ) select * from cte;
+
+-- Update Cargo via Russia
+set @@cte_max_recursion_depth =10000;
+delete from shipping_rates where shipping_method_id = 3;
+insert into shipping_rates (destination, weight, rate, shipping_method_id) 
+with recursive cte (destination, weight, rate, shipping_method_id) as (
+    select 'ru', 5000, 110000, 3
+    union
+    select 'ru', weight + 100, rate + 2200, 3 from cte where weight < 300000
+) select * from cte;
