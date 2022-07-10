9dddce34a92cc0f23cac2e4c5386a78046894bc8
ralfeus <ralfeus@gmail.com>
2022-07-10 16:28:23 +0200

    Allow management of non-taxable companies Fixes #165

--- test.py
+++ test.py
@@ -7,21 +7,21 @@ from app.jobs import *
 from app.purchase.jobs import *
 
 with create_app().app_context():
-    po = PurchaseOrder.query.get('PO-2021-04-0002-001')
-    po.status = PurchaseOrderStatus.pending
-    po.vendor = 'AtomyQuick'
-    po.company_id = 4
-    po.customer.username = '23426444'
-    po.customer.password = 'atomy#01'
-    po.purchase_restricted_products = True
-    db.session.flush()
+    # po = PurchaseOrder.query.get('PO-2021-04-0002-001')
+    # po.status = PurchaseOrderStatus.pending
+    # po.vendor = 'AtomyQuick'
+    # po.company_id = 4
+    # po.customer.username = '23426444'
+    # po.customer.password = 'atomy#01'
+    # po.purchase_restricted_products = True
+    # db.session.flush()
     # update_purchase_orders_status('PO-2021-03-0001-001')
     # browser = Browser(config=current_app.config)
 #     vendor = PurchaseOrderVendorManager.get_vendor(
 #         po.vendor,5
 #         browser=browser, config=current_app.config)
 #     vendor.post_purchase_order(po)
-    post_purchase_orders(po_id='PO-2021-04-0002-001')
+    post_purchase_orders(po_id='PO-2022-05-0002-001')
     db.session.rollback()
     # print(po.to_dict())
     # cProfile.run('build_network(root_id="S7882533", incremental=True)', filename='build_network.stat')
