7bdad21ecb3b3fb45275a09b5a494eee804f8335
Mychajlo Chodorev <ralfeus@gmail.com>
2023-06-05 18:13:35 +0200

    fix product import

--- temp.py
+++ temp.py
@@ -5,6 +5,7 @@ from app import db, create_app
 from app.purchase.models import PurchaseOrder
 from app.products.models import *
 import logging
+from app.import_products import get_atomy_products
 
 from network_builder.build_network import build_network
 logging.basicConfig(level=logging.DEBUG)
@@ -21,12 +22,13 @@ with create_app().app_context():
     # po.customer.password = 'atomy#01'
     # po.customer.username = 'S5832131'
     # po.customer.password = 'mkk03020529!!'
-    po.purchase_date = datetime(2023, 4, 3)
-    db.session.flush()
+    # po.purchase_date = datetime(2023, 4, 3)
+    # db.session.flush()
     # update_purchase_orders_status(po_id)
-    post_purchase_orders(po_id=po_id)
-    db.session.rollback()
+    # post_purchase_orders(po_id=po_id)
+    # db.session.rollback()
     # print(po.to_dict())
     # cProfile.run('build_network(root_id="S7882533", incremental=True)', filename='build_network.stat')
     # build_network(user='S5832131', password='mkk030529!', incremental=True)
     # copy_subtree(root_id='S9945812')
+    get_atomy_products()
