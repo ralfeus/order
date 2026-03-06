4928eb8ca78d1535034f81d2269c73c8bb51de39
Mychajlo Chodorev <ralfeus@gmail.com>
2026-03-06 14:58:49 +0100

    .

--- tests/utils/test_atomy.py
+++ tests/utils/test_atomy.py
@@ -13,25 +13,4 @@ class AtomyTest(BaseTestCase):
 
     def test_try_perform(self):
         pass
-
-    @patch("utils.atomy.get_json")
-    def test_get_bu_place_from_network(self, mock_get_json):
-        mock_get_json.return_value = {"center_code": "12345"}
-        result = get_bu_place_from_network("testuser")
-        self.assertEqual(result, "12345")
-
-    @patch("utils.atomy.atomy_login2")
-    @patch("utils.atomy.invoke_curl")
-    def test_get_bu_place_from_page(self, mock_curl, mock_login):
-        mock_login.return_value = "jwt_token"
-        mock_curl.return_value = ('\\"buPlace\\":\\"test\\"', None)
-        result = get_bu_place_from_page("testuser", "testpassword")
-        self.assertEqual(result, 'test')
-        mock_login.assert_called_once_with("testuser", "testpassword")
-        mock_curl.assert_called_once_with(
-            url="https://kr.atomy.com/order/sheet",
-            headers=[
-                {"Cookie": "jwt_token"},
-                {"referer": "https://kr.atomy.com/order/sheet"}],
-            retries=0
-        )
+ 
